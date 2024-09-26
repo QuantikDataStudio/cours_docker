dc3d558ab71362484ba18679dfc1f0b07ea27479
Peppa <126483961+Cornelius-BobCat@users.noreply.github.com>
2024-09-26 11:57:26 +0200

    Update main.py

--- TP1 - FastAPI/app/main.py
+++ TP1 - FastAPI/app/main.py
@@ -1,1 +1,30 @@
 
+import requests
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+file_path = "data/databse.csv"
+
+@app.get("/")
+async def home():
+    return {"home":"hello world!"}
+
+@app.get("/file")
+async def home():
+    url = "https://raw.githubusercontent.com/france-connect/data-provider-example/refs/heads/master/database.csv"
+
+    #request
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        try:
+            with open(file_path, 'wb') as f:
+                f.write(response.content)
+        except Exception as e:
+            return {"file" : str(e)}
+        
+        return {"file" : file_path}
+    else:
+        return {"file" : "il y a un probleme ", "status" : response.status_code}
